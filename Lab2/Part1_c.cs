9374a7e8c5cbc05027c351c75f30efff5fa88d0b
AndriiStetsenko <55089365+AndriiStetsenko@users.noreply.github.com>
2020-06-01 09:48:36 +0300

    Add files via upload

---
+++ Lab2/Part1_c.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace part1_c
+{
+    class Part1_c
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First number: ");
+            Int32 multiplicand = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number: ");
+            Int32 multiplier = Convert.ToInt32(Console.ReadLine());
+            Int64 product = (long)multiplier;
+            Int64 support = (long)multiplicand;
+            support <<= 32;
+
+            Console.WriteLine("Multiplicand: " + Convert.ToString(multiplicand, 2).PadLeft(32, '0'));
+            Console.WriteLine("Multiplier: " + Convert.ToString(multiplier, 2).PadLeft(32, '0'));
+            Console.WriteLine("*Assigning multiplier to right 32 bits of product*\nProduct: " + Convert.ToString(multiplier, 2).PadLeft(32, '0'));
+
+            for (int i = 0; i < 32; i++)
+            {
+                if ((product & 1) == 1)
+                {
+                    Console.WriteLine("*Last bit in product is 1 - adding*");
+                    product += support;
+                }
+                Console.WriteLine("* Shifting product right*");
+                product >>= 1;
+                Console.WriteLine(Convert.ToString(product, 2).PadLeft(64, '0'));
+            }
+            Console.WriteLine("Result = " + product);
+        }
+    }
+}
---
+++ Lab2/Part2_b.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace part2_b
+{
+    class Part2_b
+    {
+        static string generate_number(Int64 num)
+        {
+            string str = Convert.ToString(num, 2);
+            str = str.PadLeft(64, '0');
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a first number: ");
+            string remainder = Console.ReadLine();
+            Console.Write("Enter a second number: ");
+            string divisior = Console.ReadLine();
+            Int64 rem64 = Int32.Parse(remainder);
+            Int64 div64 = Int32.Parse(divisior);
+            div64 <<= 32;
+            bool remToOne = false;
+            for (int i = 0; i <= 32; i++)
+            {
+                Console.WriteLine($"\nStep {i + 1} ");
+                if (div64 <= rem64)
+                {
+                    rem64 -= div64;
+                    remToOne = true;
+                    Console.WriteLine("Divisior lediv64 than remainder");
+                }
+                else
+                    Console.WriteLine("Divisior more than remainder");
+                Console.WriteLine("Shift remainder left one bit");
+                rem64 <<= 1;
+                if (remToOne)
+                {
+                    remToOne = false;
+                    rem64 |= 1;
+                    Console.WriteLine("Set remainder right bit to 1");
+                }
+                Console.WriteLine($"Remaider: {generate_number(rem64)}");
+                Console.WriteLine($"Divisior: {generate_number(div64)}");
+            }
+            long quotient = rem64 & ((long)Math.Pow(2, 33) - 1);
+            long rem = rem64 >> 33;
+            Console.WriteLine("\n\nQuotient: " + generate_number(quotient) +
+            " ( " + quotient + " )\n");
+
+            Console.WriteLine("Remainder: " + generate_number(rem) +
+            " ( " + rem + " )");
+        }
+    }
+}
